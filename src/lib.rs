83020d31dcaf40f8eff4d4c079ce4f7d7518db76
Jobin Nelson <jobinnelson369@gmail.com>
2023-08-02 21:33:03 +0530

    refactor run_repl function to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 pub mod lexer;
 pub mod repl;
 pub mod token;
+
+pub fn run_repl() {
+    let user = std::env::var("USER").unwrap();
+    println!("Hello {}! This is Monkey programming language", user);
+    println!("Feel free to type in commands");
+
+    repl::start(io::stdin(), io::stdout()).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use std::io;
-
-use monkey_interpreter::repl;
+use monkey_interpreter::run_repl;
 
 fn main() {
-    let user = std::env::var("USER").unwrap();
-    println!("Hello {}! This is Monkey programming language", user);
-    println!("Feel free to type in commands");
-
-    repl::start(io::stdin(), io::stdout()).unwrap();
+    run_repl();
 }
